39f6ad725c4cf3a0da1247cdd0d473b83ae3eda9
Antonio Villegas <antonio.ixtapa@yahoo.com>
2026-06-12 09:31:45 -0600

    Dashboard: quitar seguimiento, agregar notas y sin exito

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3000;
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: false,
+  ssl: { rejectUnauthorized: false },
 });
 
 const XLSX_PATH   = path.join(__dirname, 'ANERA-Visitas.xlsx');
